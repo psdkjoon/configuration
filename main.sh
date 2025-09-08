6e39f5bfb8f5470d3bdd4c2547f4909f2697766a
psdk <psdkiajhn@gmail.com>
2025-09-09 00:31:14 +0330

    First commit

---
+++ main.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+AUCO_PATH=$(pwd)
+WALLPATH=/opt/wallpapers/ayu-dark.png
+USERHOME=/home/$USER
+
+yayinstall() {
+	mkdir ~/git
+	git clone https://aur.archlinux.org/yay-bin.git ~/git/yay-bin/
+	cd ~/git/yay-bin
+	sudo makepkg -si
+}
+
+sudo pacman -Syu git curl wget hyprland kitty rofi waybar mako swaylock swayidle hyprpaper xdg-desktop-portal-hyprland zsh zoxide fd ripgrep bat gping thefuck tealdeer neovim btop tgpt 7zip jq poppler fzf resvg imagemagick npm wl-clipboard mpv ttf-jetbrains-mono-nerd
+
+yayinstall
+
+yay -Sy oh-my-posh
+
+cd $AUCO_PATH
+
+tldr --update
+
+7z x configs.zip
+
+sudo mkdir $USERHOME/.pt
+
+sudo mv base.toml $USERHOME/.pt/
+
+sudo mv beep.mp3 /opt/
+
+sudo mv .zshrc $USERHOME/
+
+sudo mv nvim yazi nekoray $USERHOME/.config/
+
+sudo mkdir $USERHOME/.config/kitty
+
+sudo mv kitty.conf $USERHOME/.config/kitty/
+
+sudo mv nekobox $USERHOME/Desktop/
+
+sudo mv wallpapers /opt/
+
+sudo rm -rf nvim wallpapers yazi base.toml .zshrc nekoray nekobox beep.mp3
