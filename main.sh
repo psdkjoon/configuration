d84228b54eebcb4a49b989926d47a52e1593b4d0
psdkjoon <psdkiajhn@gmail.com>
2025-09-09 10:16:58 +0000

    updating nekoray to thorne and some bug fixes

--- main.sh
+++ main.sh
@@ -8,20 +8,22 @@ yayinstall() {
 	mkdir ~/git
 	git clone https://aur.archlinux.org/yay-bin.git ~/git/yay-bin/
 	cd ~/git/yay-bin
-	sudo makepkg -si
+	makepkg -si
 }
 
-sudo pacman -Syu git curl wget hyprland kitty rofi waybar mako swaylock swayidle hyprpaper xdg-desktop-portal-hyprland zsh zoxide fd ripgrep bat gping thefuck tealdeer neovim btop tgpt 7zip jq poppler fzf resvg imagemagick npm wl-clipboard mpv ttf-jetbrains-mono-nerd
+sudo pacman -Syu git curl wget hyprland kitty rofi waybar mako swaylock swayidle hyprpaper xdg-desktop-portal-hyprland zsh zoxide fd ripgrep bat gping thefuck tealdeer neovim btop tgpt 7zip jq poppler fzf resvg imagemagick npm wl-clipboard mpv ttf-jetbrains-mono-nerd yazi lsd
 
 yayinstall
 
-yay -Sy oh-my-posh
+yay -Sy oh-my-posh thorne-bin
 
 cd $AUCO_PATH
 
 tldr --update
 
 7z x configs.zip
+
+sudo mkdir $USERHOME/.config
 
 sudo mkdir $USERHOME/.pt
 
@@ -31,13 +33,11 @@ sudo mv beep.mp3 /opt/
 
 sudo mv .zshrc $USERHOME/
 
-sudo mv nvim yazi nekoray $USERHOME/.config/
+sudo mv nvim yazi $USERHOME/.config/
 
 sudo mkdir $USERHOME/.config/kitty
 
 sudo mv kitty.conf $USERHOME/.config/kitty/
-
-sudo mv nekobox $USERHOME/Desktop/
 
 sudo mv wallpapers /opt/
 
