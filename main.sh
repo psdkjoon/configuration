ae7cb3bc03201e1eb240eafcf8d7968ba3cb08c7
psdkjoon <psdkiajhn@gmail.com>
2025-09-30 18:57:25 +0330

    adding and fixing lots of thing, check it yourself :/

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 AUCO_PATH=$(pwd)
-WALLPATH=/opt/wallpapers/ayu-dark.png
 USERHOME=/home/$USER
 
 yayinstall() {
@@ -11,11 +10,11 @@ yayinstall() {
 	makepkg -si
 }
 
-sudo pacman -Syu git curl wget hyprland kitty rofi waybar hyprpaper xdg-desktop-portal-hyprland zsh zoxide fd ripgrep bat gping thefuck tealdeer neovim btop tgpt 7zip jq poppler fzf resvg imagemagick npm wl-clipboard mpv ttf-jetbrains-mono-nerd yazi lsd unzip yt-dlp mpd mpc rmpc sox swaync lua love gcc ntfs-3g mupdf gutenprint foomatic-db cups cups-filters cups-pdf ghostscript cliphist wl-copy wl-paste
+sudo pacman -Syu git curl wget hyprland kitty rofi waybar hyprpaper xdg-desktop-portal-hyprland zsh zoxide fd ripgrep bat gping thefuck tealdeer neovim btop tgpt 7zip jq poppler fzf resvg imagemagick npm wl-clipboard mpv ttf-jetbrains-mono-nerd yazi lsd unzip yt-dlp mpd mpc rmpc sox swaync lua love gcc ntfs-3g mupdf gutenprint foomatic-db cups cups-filters cups-pdf ghostscript cliphist qt5-wayland qt6-wayland
 
 yayinstall
 
-yay -Sy oh-my-posh thorne-bin
+yay -Sy oh-my-posh thorne-bin aseprite
 
 cd $AUCO_PATH
 
@@ -27,25 +26,35 @@ sudo mkdir $USERHOME/.config
 
 sudo mkdir $USERHOME/.pt
 
+sudo mkdir /root/.config
+
+sudo mkdir $USERHOME/.zsh
+
+sudo mkdir $USERHOME/.config/mpd
+
 sudo mv -f base.toml $USERHOME/.pt/
 
 sudo mv -f beep.mp3 /opt/
 
 sudo mv -f .zshrc $USERHOME/
 
-sudo mv -f nvim yazi $USERHOME/.config/
+sudo mv -f kitty nvim yazi waybar btop hypr cava lsd $USERHOME/.config/
 
-sudo mkdir $USERHOME/.config/kitty
+sudo mv -f arch.png /opt/
 
-sudo mv -f kitty.conf $USERHOME/.config/kitty/
-
-sudo mv -f wallpapers /opt/
-
-sudo sed -i '/^#Color/s/^#//; /^Color/a ILoveCandy' /etc/pacman.conf
-
-sudo mkdir /root/.config
+sudo mv -f catppuccin.zsh $USERHOME/.zsh/
 
 sudo mv -f sddm.conf /etc/
+
+sudo mv -f where_is_my_sddm_theme /usr/share/sddm/themes/
+
+sudo mv -f clipboard ytmd /usr/bin/
+
+sudo mv -f catppuccin-theme-mocha /usr/share/aseprite/data/extensions/
+
+sudo mv -f mpd.conf $USERHOME/.config/mpd/
+
+sudo sed -i '/^#Color/s/^#//; /^Color/a ILoveCandy' /etc/pacman.conf
 
 sudo ln -sf $USERHOME/.config/nvim /root/.config/nvim
 
@@ -53,26 +62,22 @@ sudo ln -sf $USERHOME/.zshrc /root/.zshrc
 
 sudo ln -sf $USERHOME/.pt /root/.pt
 
-sudo mv -f waybar $USERHOME/.config/
-
-sudo mv -f hypr $USERHOME/.config/
-
-sudo mv -f where_is_my_sddm_theme /usr/share/sddm/themes/
-
-sudo mv -f cava $USERHOME/.config/
-
-sudo mv -f clipboard ytmd /usr/bin/
-
 sudo chmod +x /usr/bin/ytmd
 
 sudo chmod +x /usr/bin/clipboard
-
-sudo mkdir -p $USERHOME/.config/mpd
-
-sudo mv -f mpd.conf $USERHOME/.config/mpd/
 
 sudo chown -R root:root /usr/share/sddm/themes/where_is_my_sddm_theme
 
 sudo chmod -R 755 /usr/share/sddm/themes/where_is_my_sddm_theme
 
-sudo rm -rf nvim wallpapers yazi base.toml .zshrc beep.mp3 hypr where_is_my_sddm_theme sddm.conf kitty.conf waybar cava mpd.conf ytmd clipboard
+sudo rm -rf nvim arch.png yazi base.toml .zshrc beep.mp3 hypr where_is_my_sddm_theme sddm.conf kitty.conf waybar cava mpd.conf ytmd clipboard catppuccin-theme-mocha btop lsd
+
+clear
+
+for NUM in {0..10}; do
+	echo -ne "\rShuting down in $((10 - NUM))\033[K"
+	sleep 1
+done
+
+echo -ne "\rBoom!\033[K"
+sudo shutdown now
