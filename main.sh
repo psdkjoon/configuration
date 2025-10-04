ac2cbcb1c5afbf41e08060347ef768a97cbd0530
psdkjoon <psdkiajhn@gmail.com>
2025-10-04 18:21:42 +0330

    idk :/

--- main.sh
+++ main.sh
@@ -10,7 +10,7 @@ yayinstall() {
 	makepkg -si
 }
 
-sudo pacman -Syu git curl wget hyprland kitty rofi waybar hyprpaper xdg-desktop-portal-hyprland zsh zoxide fd ripgrep bat gping thefuck tealdeer neovim btop tgpt 7zip jq poppler fzf resvg imagemagick npm wl-clipboard mpv ttf-jetbrains-mono-nerd yazi lsd unzip yt-dlp mpd mpc rmpc sox swaync lua love gcc ntfs-3g mupdf gutenprint foomatic-db cups cups-filters cups-pdf ghostscript cliphist qt5-wayland qt6-wayland
+sudo pacman -Syu git curl wget hyprland kitty rofi waybar hyprpaper xdg-desktop-portal-hyprland zsh zoxide fd ripgrep bat gping thefuck tealdeer neovim btop tgpt 7zip jq poppler fzf resvg imagemagick npm wl-clipboard mpv ttf-jetbrains-mono-nerd yazi lsd unzip yt-dlp mpd mpc rmpc sox swaync lua love gcc ntfs-3g mupdf gutenprint foomatic-db cups cups-filters cups-pdf ghostscript cliphist qt5-wayland qt6-wayland fcitx5-im fcitx5-mozc
 
 yayinstall
 
@@ -30,6 +30,10 @@ sudo mkdir /root/.config
 
 sudo mkdir $USERHOME/.zsh
 
+sudo mkdir -p $USERHOME/.local/share/fcitx5/themes/
+
+sudo mkdir -p $USERHOME/.config/fcitx5/conf/
+
 sudo mkdir $USERHOME/.config/mpd
 
 sudo mv -f base.toml $USERHOME/.pt/
@@ -38,15 +42,21 @@ sudo mv -f beep.mp3 /opt/
 
 sudo mv -f .zshrc $USERHOME/
 
+sudo mv -f profile $USERHOME/.config/fcitx5/
+
 sudo mv -f kitty nvim yazi waybar btop hypr cava lsd $USERHOME/.config/
 
-sudo mv -f arch.png /opt/
+sudo mv -f tux.png pacman.png /opt/
 
 sudo mv -f catppuccin.zsh $USERHOME/.zsh/
 
 sudo mv -f sddm.conf /etc/
 
+sudo mv -f catppuccin-mocha-mauve $USERHOME/.local/share/fcitx5/themes/
+
 sudo mv -f where_is_my_sddm_theme /usr/share/sddm/themes/
+
+sudo mv -f classicui.conf $USERHOME/.config/fcitx5/conf/
 
 sudo mv -f clipboard ytmd /usr/bin/
 
@@ -70,7 +80,7 @@ sudo chown -R root:root /usr/share/sddm/themes/where_is_my_sddm_theme
 
 sudo chmod -R 755 /usr/share/sddm/themes/where_is_my_sddm_theme
 
-sudo rm -rf nvim arch.png yazi base.toml .zshrc beep.mp3 hypr where_is_my_sddm_theme sddm.conf kitty.conf waybar cava mpd.conf ytmd clipboard catppuccin-theme-mocha btop lsd
+sudo rm -rf nvim tux.png pacman.png yazi base.toml .zshrc beep.mp3 hypr where_is_my_sddm_theme sddm.conf kitty.conf waybar cava mpd.conf ytmd clipboard catppuccin-theme-mocha btop lsd catppuccin-mocha-mauve classicui.conf
 
 clear
 
